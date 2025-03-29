066a99e3b923c3e5b75c4242e902ebe771c61640
Gaurav <Gauravsb105@gmail.com>
2025-03-29 11:39:07 +0530

    Fixed about tab

--- src/Component/NavItems/About.jsx
+++ src/Component/NavItems/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
     <>
       <div className="w-4/5 p-1 sm:p-3 md:p-4 mx-auto my-8 rounded-lg text-gray-800">
         <div className="text-left mb-10">
-          <h2 className="text-3xl mb-2 font-bold">Introduction</h2>
+          <h2 className="text-3xl mb-2 font-bold">About DSEU</h2>
           <div className="flex items-center mt-[-5px] w-[120px]">
             <div className="h-[2px] bg-blue-900 flex-1"></div>
             <div className="h-[5px] w-[50px] bg-blue-900 rounded-[10px]"></div>
@@ -26,7 +26,9 @@ const About = () => {
           <ul className="pl-6 text-gray-600 list-none">
             {aboutTheUniversity.objectives.map((objective, index) => (
               <li key={index} className="mb-2 flex flex-row items-center gap-2">
-                <ArrowBigRight />
+                <ArrowBigRight 
+                className="min-h-5 min-w-5 md:mb-0 mb-5"
+                />
                 {objective}
               </li>
             ))}
@@ -39,51 +41,6 @@ const About = () => {
           ))}
         </div>
 
-        <div className="text-left">
-          <h2 className="text-3xl mb-2 font-bold">History</h2>
-          <div className="flex items-center mt-[-5px] w-[80px]">
-            <div className="h-[2px] bg-blue-900 flex-1"></div>
-            <div className="h-[5px] w-[30px] bg-blue-900 rounded-[10px]"></div>
-            <div className="h-[2px] bg-blue-900 flex-1"></div>
-          </div>
-          <div className="my-2"></div>
-
-          <p className="my-4 text-gray-600">
-            The Delhi Skill and Entrepreneurship University was founded in 2020
-            by the Government of NCT of Delhi as a strategic initiative to
-            enhance the skill-based education landscape in India. This
-            establishment was driven by the urgent need to create a workforce
-            adept at meeting the demands of a rapidly changing economic
-            environment and to instill a culture of entrepreneurship among the
-            youth.
-          </p>
-          <p className="my-4 text-gray-600">
-            DSEU was conceptualized with a vision to foster an educational
-            ecosystem that is both innovative and responsive to industry needs.
-            The university's creation marked a milestone in Delhi's efforts to
-            improve vocational education and provide a robust platform for skill
-            development. It has since grown into a prominent institution known
-            for its practical learning approaches, industry partnerships, and
-            commitment to student success.
-          </p>
-          <p className="my-4 text-gray-600">
-            Over the years, DSEU has expanded its reach and impact, forging
-            collaborations with various industries and organizations to offer
-            students unparalleled opportunities for hands-on training,
-            internships, and real-world exposure. This emphasis on experiential
-            learning ensures that graduates are not only well-prepared for their
-            careers but also possess the entrepreneurial mindset required to
-            innovate and lead in their respective fields.
-          </p>
-          <p className="my-4 text-gray-600">
-            The university’s commitment to excellence in education and its
-            adaptive approach to curriculum design have positioned DSEU as a
-            leader in skill development and entrepreneurship education. The
-            institution continues to evolve, responding to the needs of the
-            global job market and ensuring its students are equipped with the
-            skills and knowledge to thrive in a competitive world.
-          </p>
-        </div>
       </div>
     </>
   );
