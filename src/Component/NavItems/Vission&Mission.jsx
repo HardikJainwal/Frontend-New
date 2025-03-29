25d9dc686e412cd6e649e7b7b40a7e895898dd5f
Gaurav <Gauravsb105@gmail.com>
2025-03-29 11:49:13 +0530

    Fixed vision and mission

--- src/Component/NavItems/Vission&Mission.jsx
+++ src/Component/NavItems/Vission&Mission.jsx
@@ -1,28 +1,40 @@
-import { mission } from "../../constants/VISION&MISSION.JS";
-import { vision } from "../../constants/VISION&MISSION.JS";
+import { motion } from "framer-motion";
+import { ArrowRightCircle } from "lucide-react";
+import { mission, vision, keyPrincipals } from "../../constants/VISION&MISSION.JS";
 
 const VissionMission = () => {
-  const sections = [vision, mission];
+  const sections = [vision, mission, keyPrincipals];
 
   return (
-      <div className="w-4/5 p-4 mx-auto my-8 rounded-lg text-gray-800">
-          {sections.map((section) => (
-              <div key={section.id} className="text-left">
-                  <h2 className="text-3xl mb-2 font-bold">{section.name}</h2>
-                  <div className="flex items-center mt-[-5px] w-[80px]">
-                      <div className="h-[2px] bg-blue-900 flex-1"></div>
-                      <div className="h-[5px] w-[30px] bg-blue-900 rounded-[10px]"></div>
-                      <div className="h-[2px] bg-blue-900 flex-1"></div>
-                  </div>
-                  <div className="my-2"></div>
-                  {section.content.map((paragraph, index) => (
-                      <p key={index} className="my-4 text-gray-600">
-                          {paragraph}
-                      </p>
-                  ))}
-              </div>
-          ))}
-      </div>
+    <div className="w-full max-w-4xl mx-auto my-10 mb-16 md:mb-0 px-6 text-gray-800">
+      {sections.map((section) => (
+        <motion.div
+          key={section.id}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+          className="text-left mb-10"
+        >
+          
+          <h2 className="text-3xl font-bold mb-3">{section.name}</h2>
+          <div className="flex items-center w-[100px]">
+            <div className="h-[2px] bg-blue-900 flex-1"></div>
+            <div className="h-[5px] w-[40px] bg-blue-900 rounded-[10px]"></div>
+            <div className="h-[2px] bg-blue-900 flex-1"></div>
+          </div>
+
+          
+          <div className="mt-4 space-y-3">
+            {section.content.map((paragraph, index) => (
+              <p key={index} className="text-gray-600 flex items-center gap-2">
+                <ArrowRightCircle className="text-blue-500 w-5 h-5" />
+                {paragraph}
+              </p>
+            ))}
+          </div>
+        </motion.div>
+      ))}
+    </div>
   );
 };
 
