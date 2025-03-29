01a4e154f20a50a9c291c159689051ef5f75a81f
Gaurav <Gauravsb105@gmail.com>
2025-03-29 16:30:16 +0530

    minor bom changes

--- src/assets/board-of-management/boardOfManagement.js
+++ src/assets/board-of-management/boardOfManagement.js
@@ -3,5 +3,7 @@ import girishKumar from './girish-kumar.jpg';
 import jpSaini from './jp-saini.jpg';
 import principalSecratory from './principal-secratory.jpg';
 import secratory from './secratory.jpg';
+import drpankajlathar from './drpankajlathar.jpg';
+import dttesecratory from './newsecratory.jpeg';
 
-export { deshmukh, girishKumar, jpSaini, principalSecratory, secratory };
+export { deshmukh, girishKumar, jpSaini, principalSecratory, secratory, drpankajlathar, dttesecratory };
