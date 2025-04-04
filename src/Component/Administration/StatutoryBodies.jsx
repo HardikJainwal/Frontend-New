7ed71f6612335d5e2066efd14a18d23ff2c96873
Gaurav <Gauravsb105@gmail.com>
2025-04-04 14:24:35 +0530

    structure of administrative

---
+++ src/Component/Administration/StatutoryBodies.jsx
@@ -0,0 +1,9 @@
+const StatutoryBodies = () => {
+  return (
+    <div>
+      dab dab dab
+    </div>
+  )
+}
+
+export default StatutoryBodies
---
+++ src/Component/Administration/TeamDseu.jsx
@@ -0,0 +1,9 @@
+const TeamDseu = () => {
+  return (
+    <div>
+      team dseu
+    </div>
+  )
+}
+
+export default TeamDseu
