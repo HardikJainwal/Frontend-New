86364fd2d41f5706020935b498f9e9672f68a5a1
Gaurav <Gauravsb105@gmail.com>
2025-04-14 15:13:36 +0530

    tried fixing buil error

--- src/assets/Library/index.js
+++ src/assets/Library/index.js
@@ -1,7 +1,7 @@
-import LibraryImage2 from "./LibraryImage2.png";
-import LibraryImage3 from "./LibraryImage3.png";
-import LibraryImage4 from "./LibraryImage4.png";
-import LibraryImage5 from "./LibraryImage5.png";
+import LibraryImage2 from "./libraryImage2.png";
+import LibraryImage3 from "./libraryImage3.png";
+import LibraryImage4 from "./libraryImage4.png";
+import LibraryImage5 from "./libraryImage5.png";
 
 export {
   LibraryImage2,
