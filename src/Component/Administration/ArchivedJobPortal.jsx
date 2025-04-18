e948843122211b1c9907ec2f28ee51766db00915
Abhishek <abhisheksharma22124@gmail.com>
2025-04-18 13:46:46 +0530

    bhot tagda change kar diya

--- src/Component/Administration/ArchivedJobPortal.jsx
+++ src/Component/Administration/ArchivedJobPortal.jsx
@@ -1,14 +1,18 @@
-import banner from '../../assets/job-portal.png';
-
+import ArchiveBanner from '../../assets/ArchiveBanner.jpg';
 const ArchivedJobPortal = () => {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <div className="h-full w-full object-cover">
+    <div className="flex flex-col justify-center items-center">
+      <div className="relative  md:w-full max-h-[250px]  overflow-hidden shadow-lg">
         <img
-          src={banner}
-          alt="Job Portal Banner"
-          className="w-full h-auto object-cover"
+          src={ArchiveBanner}
+          alt="Archive Banner"
+          className="w-full  h-full object-cover brightness-75"
         />
+        <div className="absolute inset-0 flex items-center justify-center">
+          <h1 className="text-white text-2xl sm:text-4xl md:text-6xl font-semibold text-center drop-shadow-2xl px-2">
+            ACADEMIC POSITIONS
+          </h1>
+        </div>
       </div>
 
       <div className="p-6">
